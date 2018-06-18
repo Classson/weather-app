2bc97db2adb7ba35740230412e30de9a69ffcc42
Alex <Alex.mikulewicz@gmail.com>
2018-06-18 19:25:01 -0400

    added sunrise api call

--- weather.js
+++ weather.js
@@ -62,3 +62,18 @@ function loaderFunct() {
 });
 }
 loaderFunct();
+
+function getSunInfo() {
+
+  dataObject = new XMLHttpRequest();
+
+  dataObject.open('GET', "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400", true);
+  dataObject.send();
+  dataObject.onload = function() {
+
+    sunInfo = JSON.parse(dataObject.responseText);
+    console.log(sunInfo);
+  }
+}
+
+getSunInfo();
